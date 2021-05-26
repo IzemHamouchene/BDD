647e6d63eea05e994e1fc4d07c3133661fc05803
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2021-05-26 19:12:47 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,7 +3,7 @@ GO
 -- Create the table 'Commandes'
 CREATE TABLE Commandes (
     NumCommande INT NOT NULL PRIMARY KEY,
-    ClientID_C INT NOT NULL FOREIGN KEY,
+    ClientID_C INT,
     Type_Livraison [NVARCHAR](20) NOT NULL,
     ETAT [NVARCHAR](20) NOT NULL
     );
