16fb60319ce620ea804d90b1f2edcbef1ed50362
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2021-05-26 19:19:22 +0100

    Update create-database.sql

--- create-database.sql
+++ create-database.sql
@@ -18,15 +18,6 @@ CREATE TABLE clients (
     );
 GO
 
--- Create the table 'Commandes'
-CREATE TABLE Commandes (
-    NumCommande INT NOT NULL PRIMARY KEY,
-    ClientID_C INT,
-    Type_Livraison [NVARCHAR](20) NOT NULL,
-    ETAT [NVARCHAR](20) NOT NULL
-    );
-GO
-
 INSERT INTO clients
 VALUES (
     1,
@@ -35,11 +26,4 @@ VALUES (
     );
 GO 
 
-INSERT INTO Commandes
-VALUES (
-    1,
-    1,
-    'Alger',
-    'Alger2',
-    );
-GO 
+
