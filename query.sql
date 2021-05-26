6933ca203bbd1b4770f3bb414c8da23e9df3e752
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2021-05-26 19:14:08 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,13 +1,5 @@
 USE Commerce2
 GO
--- Create the table 'Commandes'
-CREATE TABLE Commandes (
-    NumCommande INT NOT NULL PRIMARY KEY,
-    ClientID_C INT,
-    Type_Livraison [NVARCHAR](20) NOT NULL,
-    ETAT [NVARCHAR](20) NOT NULL
-    );
-GO
 
---SELECT Nom FROM  Clients
---GO
+SELECT Nom FROM  Clients
+GO
