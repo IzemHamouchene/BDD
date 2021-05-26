9aa28fc29776b61fda63575cf7389d157851db11
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2021-05-26 19:02:46 +0100

    Update create-database.sql

--- create-database.sql
+++ create-database.sql
@@ -28,19 +28,10 @@ GO
 
 -- Create the table 'Commandes'
 CREATE TABLE Commandes (
-    NumCommande INT NOT NULL PRIMARY KEY, -- primary key column
+    NumCommande INT NOT NULL PRIMARY KEY,
     ClientID_C INT NOT NULL FOREIGN KEY,
     Type_Livraison [NVARCHAR](20) NOT NULL,
     ETAT [NVARCHAR](20) NOT NULL
-    );
-GO
-
-INSERT INTO Commandes
-VALUES (
-    1,
-    1,
-    'Fast livraison',
-    'Fast Etat'
     );
 GO
 
